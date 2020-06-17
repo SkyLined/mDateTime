768b83a64cb36e8d676751ffa4b66492376f7c5a
SkyLined <github@skylined.nl>
2020-06-17 11:40:31 +0200

    Add dependency tests for Python

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -1,74 +1,16 @@
-import json, os, sys;
+from fTestDependencies import fTestDependencies;
+fTestDependencies();
 
-# Augment the search path to make the test subject a package and have access to its modules folder.
-sTestsFolderPath = os.path.dirname(os.path.abspath(__file__));
-sMainFolderPath = os.path.dirname(sTestsFolderPath);
-sParentFolderPath = os.path.dirname(sMainFolderPath);
-sModulesFolderPath = os.path.join(sMainFolderPath, "modules");
-asOriginalSysPath = sys.path[:];
-sys.path = [sParentFolderPath, sModulesFolderPath] + asOriginalSysPath;
-# Load product details
-oProductDetailsFile = open(os.path.join(sMainFolderPath, "dxProductDetails.json"), "rb");
+from mDebugOutput import fEnableDebugOutputForClass, fEnableDebugOutputForModule, fTerminateWithException;
 try:
-  dxProductDetails = json.load(oProductDetailsFile);
-finally:
-  oProductDetailsFile.close();
-# Save the list of names of loaded modules:
-asOriginalModuleNames = sys.modules.keys();
+  #Import the test subject
+  from fTestDate import fTestDate;
+  from fTestTime import fTestTime;
+  from fTestDateTime import fTestDateTime;
 
-__import__(dxProductDetails["sProductName"], globals(), locals(), [], -1);
-
-# Sub-packages should load all modules relative, or they will end up in the global namespace, which means they may get
-# loaded by the script importing it if it tries to load a differnt module with the same name. Obviously, that script
-# will probably not function when the wrong module is loaded, so we need to check that we did this correctly.
-asUnexpectedModules = list(set([
-  sModuleName.lstrip("_").split(".", 1)[0] for sModuleName in sys.modules.keys()
-  if not (
-    sModuleName in asOriginalModuleNames # This was loaded before
-    or sModuleName.lstrip("_").split(".", 1)[0] in (
-      [dxProductDetails["sProductName"]] +
-      dxProductDetails["asDependentOnProductNames"] +
-      [
-        # These built-in modules are expected:
-        "calendar", "datetime"
-      ]
-    )
-  )
-]));
-assert len(asUnexpectedModules) == 0, \
-      "Module(s) %s was/were unexpectedly loaded!" % ", ".join(sorted(asUnexpectedModules));
-
-#Import the test subject
-import mDateTime;
-
-# Restore the search path
-sys.path = asOriginalSysPath;
-
-# Sub-packages should load all modules relative, or they will end up in the global namespace, which means they may get
-# loaded by the script importing it if it tries to load a differnt module with the same name. Obviously, that script
-# will probably not function when the wrong module is loaded, so we need to check that we did this correctly.
-asUnexpectedModules = list(set([
-  sModuleName.lstrip("_").split(".", 1)[0] for sModuleName in sys.modules.keys()
-  if not (
-    sModuleName in asOriginalModuleNames # This was loaded before
-    or sModuleName.lstrip("_").split(".", 1)[0] in [
-      "mDateTime",
-      # These dependencies are expected:
-      # These built-in modules are expected:
-      "calendar", "datetime"
-    ]
-  )
-]));
-assert len(asUnexpectedModules) == 0, \
-      "Module(s) %s was/were unexpectedly loaded!" % ", ".join(sorted(asUnexpectedModules));
-# Restore the search path
-sys.path = asOriginalSysPath;
-
-from fTestDate import fTestDate;
-from fTestTime import fTestTime;
-from fTestDateTime import fTestDateTime;
-
-fTestDate();
-fTestTime();
-fTestDateTime();
-print "  + All tests successful.";
+  fTestDate();
+  fTestTime();
+  fTestDateTime();
+  print "  + All tests successful.";
+except Exception as oException:
+  fTerminateWithException(oException, bShowStacksForAllThread = True);
